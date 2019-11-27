f2028c00fe4f22854e04140aeea805328a0a8bbf
Patricia <prw250@nyu.edu>
2019-11-27 12:56:18 +0800

    basics and timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let promptItems= ["Write 5 things you are most grateful for today.", "xyz",]
+ 
+
 function App() {
+  const [quote, setQuote] = useState();
+  const [remainingtime, setRemainingtime] = useState(300);
+  function randomquote(){
+    console.log(Math.floor(Math.random() * 2));
+    return(
+      setQuote(Math.floor(Math.random() * 2))
+    )
+  }
+ useEffect( 
+function startTimer(duration, display) {
+    var timer = duration;
+    setInterval(function () {
+      console.log(remainingtime)
+      setRemainingtime(remainingtime-1)
+        
+    }, 1000);
+
+    console.log(display);
+}
+ )
+
+  function handleclick() {
+    let  fiveMinutes = 60 * 5,
+    display = document.querySelector('#time');
+
+   randomquote();
+  //  startTimer(fiveMinutes, display);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" >
+        <h1>Guided Positivity Break</h1>
+        <span id="time">{remainingtime}</span>
+        <button onClick={handleclick}>BEGIN</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {promptItems[quote]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
+
+
+
 
 export default App;

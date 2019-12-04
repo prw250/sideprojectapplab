58235e4cbbdd9c708547d1b895e495fa5719734d
Patricia <prw250@nyu.edu>
2019-12-04 12:00:45 +0800

    previous database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { useState , useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 let promptItems= ["Write 5 things you are most grateful for today.", "What is something that always puts you in a good mood?", "Write about a time when a situation that seemed bad turned out okay in the end.","What is your favorite thing about yourself?","Tell about a time you were grateful for something a friend did for you.","What’s something that you’re looking forward to?","What’s a simple pleasure that you’re grateful for?","What’s something that you grateful to have today that you didn’t have a year ago?", "Write about a happy memory.","Write about someplace you’ve been that you’re grateful for.", "What’s something about your body or health that you’re grateful for?", "What’s an accomplishment you’re proud of?", "Open the door or window and look outside. What’s something you’re grateful for outside?", "Open your phone or photo album and find a photo that you like. Why are you grateful for this photo? What are you grateful for in the photo?", "What have you been given that you’re grateful for?","Write about some of your best qualities", "What would your teenage self love about you now?",]
  
+var firebaseConfig = {
+  apiKey: "AIzaSyCe4-5423X8sV1INwyp8BG57JbpQwHuTmA",
+  authDomain: "sideprojectapplab.firebaseapp.com",
+  databaseURL: "https://sideprojectapplab.firebaseio.com",
+  projectId: "sideprojectapplab",
+  storageBucket: "sideprojectapplab.appspot.com",
+  messagingSenderId: "989661843128",
+  appId: "1:989661843128:web:dfda055533053f3d10ae4d",
+  measurementId: "G-GDZV69EXYV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 function App() {
   const [quote, setQuote] = useState();
   const [remainingtime, setRemainingtime] = useState(300);
   const [buttonclicked, setButtonclicked] = useState(false);
+  const [message,setMessage]= useState("");
+  const [messages,setMessages]= useState({});
   function randomquote(){
     console.log(Math.floor(Math.random() * 17));
     return(
@@ -16,6 +32,11 @@ function App() {
     )
   }
   useEffect(() => {
+    var Ref = firebase.database().ref("messages");
+Ref.on('value', function(snapshot) {
+  console.log(snapshot.val());
+  setMessages(snapshot.val());
+});
     const id = setInterval(() => {
       if (buttonclicked) setRemainingtime(n => n - 1)}, 1000);
     return () => clearInterval(id);
@@ -30,6 +51,13 @@ function App() {
   //  startTimer(fiveMinutes, display);
   }
 
+  function handleSubmit(){
+    firebase.database().ref("messages").push(message)
+  }
+  function handleChange(event){
+    setMessage(event.target.value);
+  }
+
   return (
     <div className="App" >
         <h1>Guided Positivity Break</h1>
@@ -38,7 +66,11 @@ function App() {
         <p>
           {promptItems[quote]}
         </p>
-        <textarea rows="10" cols="55">Patricia:</textarea>
+        <textarea rows="10" cols="55" onChange={handleChange}>{message}</textarea>
+        <button onClick={handleSubmit}>Submit</button>
+        {Object.keys(messages).map(k => {
+          return <div>{messages[k]}</div>;
+        })}
     </div>
 
   );
